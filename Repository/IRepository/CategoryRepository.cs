4e06186c02678a0a05d90c62e1bab0a0e06d752f
Munna Kumar <munna@inestweb.com>
2025-02-03 18:16:02 +0530

    Section 12 - Toastr Notifications

---
+++ Repository/IRepository/CategoryRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YumBlazor.Data;
+
+namespace YumBlazor.Repository.IRepository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) {
+            _db = db;
+        }
+        public async Task<Category> CreateCategoryAsync(Category category)
+        {
+            await _db.Category.AddAsync(category);
+            await _db.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<bool> DeleteCategoryAsync(int id)
+        {
+            int count = 0;
+            var category = _db.Category.Find(id);
+            if (category != null) {
+                _db.Category.Remove(category);
+                count = await _db.SaveChangesAsync();
+            }
+            return count > 0;
+        }
+
+        public async Task<Category> GetAsync(int categoryId)
+        {
+            int count = 0;  
+            var category = await _db.Category.FindAsync(categoryId);
+            if (category != null)
+            {
+                count = _db.SaveChanges();
+            }
+            return category;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _db.Category.ToListAsync();
+        }
+
+        public async Task<Category> UpdateCategoryAsync(Category category)
+        {
+            int count = 0;
+            var obj = await _db.Category.FirstOrDefaultAsync(u => u.Id == category.Id);
+            if (obj is not null)
+            {
+                obj.Name = category.Name;
+                _db.Category.Update(obj);   
+                count = await _db.SaveChangesAsync();
+            }
+            return obj;
+        }
+    }
+}
---
+++ Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using YumBlazor.Data;
+
+namespace YumBlazor.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public Task<Category> GetAsync(int categoryId);
+        public Task<IEnumerable<Category>> GetAllAsync();
+        public Task<Category> CreateCategoryAsync(Category category);
+        public Task<Category> UpdateCategoryAsync(Category category);
+        public Task<bool> DeleteCategoryAsync(int id);
+    }
+}
