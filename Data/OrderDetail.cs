e9e72e32e2c5b3a1b9c41950f52ab92306e4865e
Munna Kumar <munna@inestweb.com>
2025-02-11 19:32:04 +0530

    Add to cart, cart list, checkout, order detail, order list updated

---
+++ Data/OrderDetail.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YumBlazor.Data
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+        public int OrderHeaderId { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        [Required]
+        public int Count { get; set; }
+
+        public double Price { get; set; }
+
+        [Required]
+        public string ProductName { get; set; } 
+    }
+}
